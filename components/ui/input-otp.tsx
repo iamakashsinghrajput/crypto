b32e0bd7519b45bcab302e406e8c800a8d4a7f29
Akash Singh <akash21052000singh@gmail.com>
2025-04-03 04:00:38 +0530

    second commit

--- components/ui/input-otp.tsx
+++ components/ui/input-otp.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import * as React from 'react';
-import { OTPInput, OTPInputContext } from 'input-otp';
+import { OTPInput} from 'input-otp';
 import { Dot } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
@@ -34,25 +34,24 @@ const InputOTPSlot = React.forwardRef<
   React.ElementRef<'div'>,
   React.ComponentPropsWithoutRef<'div'> & { index: number }
 >(({ index, className, ...props }, ref) => {
-  const inputOTPContext = React.useContext(OTPInputContext);
-  const { char, hasFakeCaret, isActive } = inputOTPContext.slots[index];
+  // const inputOTPContext = React.useContext(OTPInputContext);
+  // const { char, hasFakeCaret, isActive } = inputOTPContext.slots[index];
 
   return (
     <div
       ref={ref}
       className={cn(
         'relative flex h-10 w-10 items-center justify-center border-y border-r border-input text-sm transition-all first:rounded-l-md first:border-l last:rounded-r-md',
-        isActive && 'z-10 ring-2 ring-ring ring-offset-background',
         className
       )}
       {...props}
     >
-      {char}
+      {/* {char}
       {hasFakeCaret && (
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
           <div className="h-4 w-px animate-caret-blink bg-foreground duration-1000" />
         </div>
-      )}
+      )} */}
     </div>
   );
 });
